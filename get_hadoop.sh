c9df4cafa6b6fe65d991a36d456440e4f17f23e1
bupticybee <icybee@bupt.edu.cn>
2017-04-05 13:22:06 +0800

    Update get_hadoop.sh

--- get_hadoop.sh
+++ get_hadoop.sh
@@ -3,16 +3,18 @@ HADOOP_V=2.7.3
 #if [ "$?" -ne 0 ]; then echo "command failed"; exit 1; fi
 #sudo tar -zxf hadoop-${HADOOP_V}.tar.gz -C /usr/local
 #if [ "$?" -ne 0 ]; then echo "command failed"; exit 1; fi
-scp hadoop@hadoop.buptcloud.info:~/hadoop.master.tar.gz .
-if [ "$?" -ne 0 ]; then echo "command failed"; exit 1; fi
+
 #cd /usr/local/
 #sudo mv ./hadoop-${HADOOP_V}/ ./hadoop
-sudo tar -zxf hadoop-master.tar.gz -C /usr/local
+scp hadoop@hadoop.buptcloud.info:~/hadoop.master.tar.gz .
 if [ "$?" -ne 0 ]; then echo "command failed"; exit 1; fi
-sudo chown -R hadoop ./hadoop
+sudo tar -zxf hadoop.master.tar.gz -C /usr/local
+if [ "$?" -ne 0 ]; then echo "command failed"; exit 1; fi
+sudo chown -R hadoop /usr/local/hadoop
 if [ "$?" -ne 0 ]; then echo "command failed"; exit 1; fi
 
-wget http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz
+#wget http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz --no-check-certificate
+wget http://mirrors.linuxeye.com/jdk/jdk-8u121-linux-x64.tar.gz --no-check-certificate
 if [ "$?" -ne 0 ]; then echo "command failed"; exit 1; fi
 sudo tar -zxf jdk-8u121-linux-x64.tar.gz -C /usr/local
 if [ "$?" -ne 0 ]; then echo "command failed"; exit 1; fi
