4a655308ac54a212ecc09ff9bb6b6d922eee2e31
bupticybee <icybee@bupt.edu.cn>
2017-04-05 13:29:33 +0800

    Update get_hadoop.sh

--- get_hadoop.sh
+++ get_hadoop.sh
@@ -18,3 +18,7 @@ wget http://mirrors.linuxeye.com/jdk/jdk-8u121-linux-x64.tar.gz --no-check-certi
 if [ "$?" -ne 0 ]; then echo "command failed"; exit 1; fi
 sudo tar -zxf jdk-8u121-linux-x64.tar.gz -C /usr/local
 if [ "$?" -ne 0 ]; then echo "command failed"; exit 1; fi
+
+# get ssh keys from master
+mkdir -p ~/.ssh/
+scp hadoop@hadoop.buptcloud.info:~/.ssh/authorized_keys ~/.ssh/authorized_keys
